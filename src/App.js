294d51d9049e9d720dd5d2f895e8e4b192ca2f79
Ivan Ananev <ivcheg@gmail.com>
2019-04-04 17:37:53 +0300

    move TimerPresenter to components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import TimerPresenter from './components/TimerPresenter';
 
 const App = () => {
   const [isOn, setIsOn] = useState(false);
@@ -73,34 +74,34 @@ const App = () => {
   );
 };
 
-const TimerPresenter = ({ timer }) => {
-  const addZeros = (numStr, length = 2) => {
-    if (numStr.length >= length) {
-      return numStr;
-    }
-    return addZeros(`0${numStr}`, length);
-  };
+// const TimerPresenter = ({ timer }) => {
+//   const addZeros = (numStr, length = 2) => {
+//     if (numStr.length >= length) {
+//       return numStr;
+//     }
+//     return addZeros(`0${numStr}`, length);
+//   };
 
-  const timerMinutes = () => {
-    const minutes = Math.floor(timer / 60000) % 60;
-    return addZeros(minutes.toString());
-  };
+//   const timerMinutes = () => {
+//     const minutes = Math.floor(timer / 60000) % 60;
+//     return addZeros(minutes.toString());
+//   };
 
-  const timerSeconds = () => {
-    const seconds = Math.floor(timer / 1000) % 60;
-    return addZeros(seconds.toString());
-  };
+//   const timerSeconds = () => {
+//     const seconds = Math.floor(timer / 1000) % 60;
+//     return addZeros(seconds.toString());
+//   };
 
-  const timerMilliseconds = () => {
-    const milleseconds = timer % 1000;
-    return addZeros(milleseconds.toString(), 3);
-  };
+//   const timerMilliseconds = () => {
+//     const milleseconds = timer % 1000;
+//     return addZeros(milleseconds.toString(), 3);
+//   };
 
-  return (
-    <h1>
-      {timerMinutes()}:{timerSeconds()}.{timerMilliseconds()}
-    </h1>
-  );
-};
+//   return (
+//     <h1>
+//       {timerMinutes()}:{timerSeconds()}.{timerMilliseconds()}
+//     </h1>
+//   );
+// };
 
 export default App;
